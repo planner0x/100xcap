88f91a215ba8d126b393a5c12b37745ef17a6629
planner0x <98565934+planner0x@users.noreply.github.com>
2022-01-28 03:47:45 +0100

    Add files via upload

---
+++ 0.hedge1/quantcert/sma.py
@@ -0,0 +1,11 @@
+#
+# Python Module with Class
+# for Vectorized Backtesting
+# of Linear Regression-Based Strategies
+#
+# Python for Algorithmic Trading
+# (c) Dr. Yves J. Hilpisch
+# The Python Quants GmbH
+#
+import numpy as np
+import pandas as pd
